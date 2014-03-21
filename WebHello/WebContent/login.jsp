d71abe8d6c8411ca26f9e85a1fb9abc9cdec9174
Raul <raulssj2@gmail.com>
2014-03-21 13:08:18 +0100

    Initial checking of the login form where there is no database used yet.

---
+++ WebHello/WebContent/login.jsp
@@ -0,0 +1,21 @@
+<%@page import="sun.usagetracker.UsageTrackerClient"%>
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Login</title>
+</head>
+<body>
+<h1>Página de login.jsp</h1>
+
+<%
+   String usuario = (String) request.getParameter("usuario");
+   String clave = (String) request.getParameter("clave");
+%>
+<h2>USUARIO: <%=usuario%></h2>
+<h2>CONTRASEÑA: <%=clave%></h2>
+
+</body>
+</html>
---
+++ WebHello/src/com/raul/servlets/LoginServlet.java
@@ -0,0 +1,46 @@
+package com.raul.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet(name = "accionLoginServlet", urlPatterns = { "/accionLoginServlet" })
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String user = (String) request.getParameter("usuario");
+		String pwd = (String) request.getParameter("clave");
+		System.out.println("\t\tSERVLET: " + user + "-" + pwd);
+		response.sendRedirect("aplicacion/pagina_inicio.jsp");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
